2e6b649a9410822935f55511cbe102240f5020a3
jameshawrych <69646550+jameshawrych@users.noreply.github.com>
2022-06-14 20:51:43 +0100

    adding devops project

--- React-Website/src/components/projectdata.js
+++ React-Website/src/components/projectdata.js
@@ -21,6 +21,14 @@ export const ProjectData = [
         description:"Having seen the performace and quality of React websites, I decided to build by own to showcase my projects. To do this I used React and Javascript alongside HTML and CSS. You can see the results by navigating around this website!",
         tags:["React", "HTML", "CSS"],
     },
+    {
+        id:4,
+        name:"DevOps Website Integration",
+        description:"After a couple of manual deployments I realised that is was quite labourious to push new versions of my website to the server. Therefore I decided to try my hand at DevOps and integrate my website with CircleCI.\
+        I was able to automate the build, testing and deployment of my website using a yaml configuration file that runs commands on an ephemeral VM that CircleCI spins up when the pipeline runs. The final deployment is acheived by\
+        by connection to the server via SSH and remotely syncing the files that have just been built in the pipeline to the server.  ",
+        tags:["DevOps", "CircleCI", "Automation", "CI/CD"],
+    },
  
   
 ] 
